b236b704c82da5d35624ed355c059e96028addc9
DaveMatNat <davidmatthewnathanson@gmail.com>
2026-01-06 22:19:52 -0500

    Solved Longest Substring Without Repeating Characters

--- 03-Sliding-Window/02-Longest-Substring-Without-Repeating-Characters/solution.py
+++ 03-Sliding-Window/02-Longest-Substring-Without-Repeating-Characters/solution.py
@@ -6,20 +6,33 @@ Problem:
 
 Approach:
 
-Time Complexity: O(?)
-Space Complexity: O(?)
+Time Complexity: O(n)
+Space Complexity: O(m)
 """
 
 class Solution:
-    def solve(self):
-        # TODO: Implement solution
-        pass
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charSet = set()
+        maxLen = 0
+        l = 0
+
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            maxLen = max(maxLen, r - l + 1)
+        return maxLen
+            
+                
 
 
 # Test cases
 if __name__ == "__main__":
     solution = Solution()
     
-    # Test case 1
+    # Test cases
+    print(solution.lengthOfLongestSubstring("abcabcbb"))
+    print(solution.lengthOfLongestSubstring("bbbbb"))
     # TODO: Add test cases
     pass
