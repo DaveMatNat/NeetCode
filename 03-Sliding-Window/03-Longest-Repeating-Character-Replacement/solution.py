6fd42ecc8bbfd757c94edf4c165b6fbf36831aef
DaveMatNat <davidmatthewnathanson@gmail.com>
2026-01-08 23:38:30 -0500

    solved longest repeating Character Replacement

--- 03-Sliding-Window/03-Longest-Repeating-Character-Replacement/solution.py
+++ 03-Sliding-Window/03-Longest-Repeating-Character-Replacement/solution.py
@@ -6,14 +6,24 @@ Problem:
 
 Approach:
 
-Time Complexity: O(?)
-Space Complexity: O(?)
+Time Complexity: O(26 * n)
+Space Complexity: O(26)
 """
 
 class Solution:
-    def solve(self):
-        # TODO: Implement solution
-        pass
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {}
+        res = 0
+
+        l = 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+
+            while (r - l + 1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l += 1
+            res = max(res, r - l + 1)
+        return res
 
 
 # Test cases
@@ -21,5 +31,7 @@ if __name__ == "__main__":
     solution = Solution()
     
     # Test case 1
+    print(solution.characterReplacement("AAABABB",1))
+    print(solution.characterReplacement("XYYX",2))
     # TODO: Add test cases
     pass
