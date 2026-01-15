8d775ed040b113454b4cae02d639950112e7da86
DaveMatNat <davidmatthewnathanson@gmail.com>
2026-01-14 23:45:13 -0500

    Solved Daily temp

--- 04-Stack/05-Daily-Temperatures/solution.py
+++ 04-Stack/05-Daily-Temperatures/solution.py
@@ -6,20 +6,41 @@ Problem:
 
 Approach:
 
-Time Complexity: O(?)
-Space Complexity: O(?)
+Time Complexity: O(n)
+Space Complexity: O(n)
 """
 
 class Solution:
-    def solve(self):
-        # TODO: Implement solution
-        pass
+    def dailyTemperatures(self, temperatures: list) -> list:
+        # Result list to store how many days until a warmer temperature
+        res = [0] * len(temperatures)
+
+        # Stack to store pairs of (temperature, index)
+        # We'll use this to find the next warmer day
+        stack = []
+
+        # Go through each day and its temperature
+        for i, t in enumerate(temperatures):
+
+            # While the current temperature is warmer than the last one in the stack:
+            # It means we've found a "next warmer day" for that earlier day
+            while stack and t > stack[-1][0]:
+                prevTemp, prevIdx = stack.pop()
+                res[prevIdx] = i - prevIdx  # Calculate how many days it took to get warmer
+
+            # Add the current day to the stack to find its next warmer day later
+            stack.append((t, i))
+
+        # Return the final result list
+        return res
 
 
 # Test cases
 if __name__ == "__main__":
     solution = Solution()
     
-    # Test case 1
+    # Test cases
+    print(solution.dailyTemperatures([30,38,30,36,35,40,28]))
+    print(solution.dailyTemperatures([22,21,20]))
     # TODO: Add test cases
     pass
