f89b0ba4562abd5ec7610307536ea350e9d54b09
DaveMatNat <davidmatthewnathanson@gmail.com>
2026-01-20 23:14:14 -0500

    Solved Binary Search

--- 05-Binary-Search/01-Binary-Search/solution.py
+++ 05-Binary-Search/01-Binary-Search/solution.py
@@ -6,19 +6,29 @@ Problem:
 
 Approach:
 
-Time Complexity: O(?)
-Space Complexity: O(?)
+Time Complexity: O(log(n))
+Space Complexity: O(1)
 """
 
 class Solution:
-    def solve(self):
-        # TODO: Implement solution
-        pass
+    def search(self, nums: list, target: int) -> int:
+        l,r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l+r) //2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return -1
 
 
 # Test cases
 if __name__ == "__main__":
     solution = Solution()
+
+    print(solution.search([-1,0,2,4,6,8],4))
     
     # Test case 1
     # TODO: Add test cases
